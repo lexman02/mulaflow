27431a0bc52dface0da44b519d6e6a8757700f0d
lexman02 <10473497+lexman02@users.noreply.github.com>
2023-11-18 17:50:40 -0500

    implement upcoming expenses

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,8 +8,10 @@ from budget.models import Budget
 
 @login_required
 def get_dashboard(request):
-    current_date = datetime.date.today()
-    # Get the authenticated user's budget for the current month, assuming your logic to retrieve it
+    expenses = []
+    current_date = datetime.date.today().replace(day=15)
+
+    # Get the authenticated user's budget for the current month
     try:
         # Query the Budget model to get the user's budget for the current month
         budget = Budget.objects.get(user=request.user.id, created__month=current_date.month,
@@ -17,8 +19,16 @@ def get_dashboard(request):
 
         budget_totals = Budget.objects.current_budget_totals(request.user)
 
+        # Order the expenses by due date moving the ones without a due date or that have passed to the end
+        for expense in budget.expenses.all():
+            if expense.due_date < current_date:
+                expenses.append(expense)
+            else:
+                expenses.insert(0, expense)
+
         context = {
-            'budget': budget,
+            'expenses': expenses,
+            'today': current_date,
             'budget_totals': budget_totals,
         }
 
